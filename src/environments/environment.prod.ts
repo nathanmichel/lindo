a1d3b8061feac8449d2987e656869f1ec520e727
Nathan <nathan.michel@epitech.eu>
2020-12-24 16:46:37 +0100

    Prod firebase conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,14 @@ export const environment = {
   production: true,
   appName: "Lindo",
   websiteUrl: "http://lindo.no-emu.co",
-  apiUrl: "http://api.no-emu.co"
+  apiUrl: "http://api.no-emu.co",
+  firebaseTerracogita: {
+    apiKey: "AIzaSyCmK27yYcr2Ap2VBFtY1OjBTxtjJl_1UjI",
+    authDomain: "dt-price-farm-1.firebaseapp.com",
+    databaseURL: "https://dt-price-farm-1.firebaseio.com",
+    projectId: "dt-price-farm-1",
+    storageBucket: "dt-price-farm-1.appspot.com",
+    messagingSenderId: "787288518512",
+    appId: "1:787288518512:web:a7e319fce96d30160029b5"
+  }
 };
